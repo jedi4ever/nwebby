78a91426609cf65ca92c518d4086b6d7b1626f8c
Patrick Debois <Patrick.Debois@jedi.be>
2014-03-24 10:15:28 +0100

    added partials support

---
+++ test/partials.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var expect = require('expect.js');
+var nwebby = require('..');
+var path = require('path');
+
+var fixturize = function(baseDir, filepath) {
+  return {
+    filename: path.join(__dirname, 'fixtures', baseDir, filepath),
+    options: {
+      baseDir: path.join(__dirname, 'fixtures', baseDir)
+    }
+  };
+};
+
+describe('Partials', function() {
+
+  it('should find all partials', function(done) {
+    var fixture = fixturize('simple','variable_from_directory_yaml.txt');
+    nwebby.readPartials(fixture.options.baseDir, function(err,result) {
+      expect(err).to.be(null);
+      var expected = {
+       '/partial1': 'this is partial1\n',
+       '/partial2/partial2': 'this is partial2\n'
+      };
+      expect(result).to.eql(expected);
+      done();
+    });
+  });
+
+});
