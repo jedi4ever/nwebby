dac4848ad95f58f794cf86135e1eca62c3f54c15
Patrick Debois <Patrick.Debois@jedi.be>
2014-03-30 19:17:54 +0200

    introduce page.url && check if metadata is hash

--- test/metadata_test.js
+++ test/metadata_test.js
@@ -7,11 +7,25 @@ var path = require('path');
 var fullPath = function(filepath) {
   return path.join(__dirname, 'fixtures', filepath);
 };
+var options = {
+  baseDir: path.join(__dirname,'fixtures'),
+  templateDir: path.join(__dirname, 'fixtures', 'templates')
+};
+
+var fixturize = function(baseDir, filepath) {
+  return {
+    filename: path.join(__dirname, 'fixtures', baseDir, filepath),
+    options: {
+      baseDir: path.join(__dirname, 'fixtures', baseDir),
+      templateDir: path.join(__dirname, 'fixtures', 'templates')
+    }
+  };
+};
 
 describe('Metadata Detection', function() {
   it('should error on a non-existing file', function(done) {
     var filename = fullPath('non-existing-file.txt');
-    nwebby.getMetadata(filename,function(err) {
+    nwebby.getMetadata(filename,options,function(err) {
       expect(err).not.to.be(null);
       done();
     });
@@ -19,10 +33,12 @@ describe('Metadata Detection', function() {
 
   it('should detect on file_with_metadata.txt', function(done) {
     var filename = fullPath('file_with_metadata.txt');
-    nwebby.getMetadata(filename,function(err,metadata) {
+    nwebby.getMetadata(filename,options, function(err,metadata) {
       expect(err).to.be(null);
       expect(metadata).to.eql({
-        title: 'hello'
+        layout: 'default',
+        title: 'hello',
+        url: '/file_with_metadata.txt'
       });
       done();
     });
@@ -30,35 +46,45 @@ describe('Metadata Detection', function() {
 
   it('should not detect on file_without_metadata.txt', function(done) {
     var filename = fullPath('file_without_metadata.txt');
-    nwebby.getMetadata(filename,function(err,metadata) {
+    nwebby.getMetadata(filename,options, function(err,metadata) {
       expect(err).to.be(null);
-      expect(metadata).to.eql({});
+      expect(metadata).to.eql({ 
+        layout: 'default',
+        url: '/file_without_metadata.txt' 
+      });
       done();
     });
   });
+
   it('should not detect on file_with_partial_metadata.txt', function(done) {
     var filename = fullPath('file_with_partial_metadata.txt');
-    nwebby.getMetadata(filename,function(err,metadata) {
+    nwebby.getMetadata(filename,options, function(err,metadata) {
       expect(err).to.be(null);
-      expect(metadata).to.eql({});
+      expect(metadata).to.eql({
+        layout: 'default',
+        url: '/file_with_partial_metadata.txt'
+      });
       done();
     });
   });
 
   it('should not detect on file_with_separator_not_in_beginning.txt', function(done) {
     var filename = fullPath('file_with_separator_not_in_beginning.txt');
-    nwebby.getMetadata(filename,function(err,metadata) {
+    nwebby.getMetadata(filename,options, function(err,metadata) {
       expect(err).to.be(null);
-      expect(metadata).to.eql({});
+      expect(metadata).to.eql({
+        layout: 'default',
+        url: '/file_with_separator_not_in_beginning.txt'
+      });
       done();
     });
   });
 
   it('should not error on file_with_unfinished_field.txt', function(done) {
     var filename = fullPath('file_with_unfinished_field.txt');
-    nwebby.getMetadata(filename,function(err,metadata) {
-      expect(err).to.be(null);
-      expect(metadata).to.eql('bla');
+    nwebby.getMetadata(filename,options,function(err,metadata) {
+      expect(err).not.to.be(null);
+      expect(metadata).to.be(undefined);
       done();
     });
   });
@@ -74,11 +100,26 @@ describe('Metadata Detection', function() {
 
   it('should not error on file_with_separator+trailingspaces', function(done) {
     var filename = fullPath('file_with_separator+trailingspaces.txt');
-    nwebby.getMetadata(filename,function(err,metadata) {
+    nwebby.getMetadata(filename,options, function(err,metadata) {
       expect(err).to.be(null);
-      expect(metadata).to.eql('bla');
+      expect(metadata.title).to.be('bla');
       done();
     });
   });
+
+  it('should not error on file_with_layout_metadata', function(done) {
+    var fixture = fixturize('simple','file_with_layout_metadata.txt');
+    nwebby.getMetadata(fixture.filename,fixture.options, function(err, result) {
+      expect(err).to.be(null);
+      expect(result).to.eql({
+        extension: 'html',
+        layout: 'layout_with_metadata',
+        url: '/file_with_layout_metadata.html',
+        title: 'a page',
+      });
+      done();
+    });
+  });
+
 
 });
